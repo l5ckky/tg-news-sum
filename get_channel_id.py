9cc855f769f8b6d1df9e4357a254a253a5351e91
Ivan <luckytv211209@gmail.com>
2025-05-02 15:18:11 +0900

    update get channel id

--- get_channel_id.py
+++ get_channel_id.py
@@ -4,13 +4,19 @@ import telethon.client
 from telethon.sync import TelegramClient
 from telethon.errors import SessionPasswordNeededError
 from telethon.client import DialogMethods
+from telethon.types import Dialog
 import config
 
 
 def get_channel_id(client, channel_name):
     try:
         dialogs: DialogMethods = client.get_dialogs()
-        print(dialogs)
+        for dialog in dialogs.iter_dialogs():
+            # dialog: Dialog
+            try:
+                print(dialog.name)
+            except Exception as er:
+                print(dialog)
         # print(f"Название: {entity.title}")
         # print(f"ID: {entity.id}")
         # print(f"Username: @{entity.username}" if entity.username else "Username: None")
